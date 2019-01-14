4f7ea646e96bfc7c3fe27a2205bd81513c53b042
Apurba <apurba10024@gmail.com>
2019-01-14 21:15:06 +0600

    2.2 Added picture taking

--- app/src/main/java/com/example/apurba/authspeechtext/PhotoActivity.java
+++ app/src/main/java/com/example/apurba/authspeechtext/PhotoActivity.java
@@ -1,10 +1,16 @@
 package com.example.apurba.authspeechtext;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class PhotoActivity extends AppCompatActivity {
+    private static final int REQ_CODE_TAKE_PICTURE = 30210;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,20 @@ public class PhotoActivity extends AppCompatActivity {
     }
 
     public void takePhotoClick(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQ_CODE_TAKE_PICTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == REQ_CODE_TAKE_PICTURE
+                && resultCode == RESULT_OK) {
+            Bitmap bmp = (Bitmap) intent.getExtras().get("data");
+            ImageView img =  findViewById(R.id.iv_photo);
+            img.setImageBitmap(bmp);
+        }
     }
 }
