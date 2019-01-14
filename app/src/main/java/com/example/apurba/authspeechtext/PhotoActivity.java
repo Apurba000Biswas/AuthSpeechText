91d384570fa92153cee56488e94a21019ffd9c9b
Apurba <apurba10024@gmail.com>
2019-01-14 21:36:00 +0600

    2.3 Added photo selection from galary

--- app/src/main/java/com/example/apurba/authspeechtext/PhotoActivity.java
+++ app/src/main/java/com/example/apurba/authspeechtext/PhotoActivity.java
@@ -2,14 +2,20 @@ package com.example.apurba.authspeechtext;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.FileNotFoundException;
+
 public class PhotoActivity extends AppCompatActivity {
     private static final int REQ_CODE_TAKE_PICTURE = 30210;
+    private static final int REQ_CODE_PHOTO_GALLERY = 30211;
 
 
     @Override
@@ -33,6 +39,22 @@ public class PhotoActivity extends AppCompatActivity {
             Bitmap bmp = (Bitmap) intent.getExtras().get("data");
             ImageView img =  findViewById(R.id.iv_photo);
             img.setImageBitmap(bmp);
+        }else if (requestCode == REQ_CODE_PHOTO_GALLERY && resultCode == RESULT_OK){
+            Uri targetUri = intent.getData();   // location of photo file
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(
+                        getContentResolver().openInputStream(targetUri));
+                ImageView img =  findViewById(R.id.iv_photo);
+                img.setImageBitmap(bitmap);
+            } catch (FileNotFoundException fnfe) {
+                Log.wtf("onActivityResult", fnfe);
+            }
         }
+    }
+
+    public void selectPhotoFromGallery(View view) {
+        Intent picIntent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(picIntent, REQ_CODE_PHOTO_GALLERY);
     }
 }
